5a6c623d26c67bcdc3d63e3be20543b642a0e502
wb_gaozy20200923 <ziyuan_gao@163.com>
2020-10-19 16:20:23 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,7 @@
 
 <script>
 export default {
-  name: 'App',
-  mounted() {
-    /**
-     * 解决 css 引入图片在 github pages 无法获取的问题
-     */
-    const { NODE_ENV } = process.env;
-    document.documentElement.className = NODE_ENV;
-  }
+  name: 'App'
 }
 </script>
 
